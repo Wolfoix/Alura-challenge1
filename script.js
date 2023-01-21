b8eddc50f93923b09e4e229848927d2c8ed3d011
Andres <vacacastillo@gmail.com>
2023-01-21 09:44:46 -0500

    Se agrega el proyecto

---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+function encriptar() {
+
+    var textuser = document.getElementById("usertxt").value.toLowerCase();
+
+    var textcoded = textuser.replace(/e/igm,"enter");
+    var textcoded = textcoded.replace(/o/igm,"ober");
+    var textcoded = textcoded.replace(/i/igm,"imes");
+    var textcoded = textcoded.replace(/a/igm,"ai");
+    var textcoded = textcoded.replace(/u/igm,"ufat");
+
+    document.getElementById("txtencriptado").innerHTML = textcoded;
+
+}
+
+function desencriptar() {
+
+    var textuser = document.getElementById("usertxt").value.toLowerCase();
+
+    var textcoded = textuser.replace(/enter/igm,"e");
+    var textcoded = textcoded.replace(/ober/igm,"o");
+    var textcoded = textcoded.replace(/imes/igm,"i");
+    var textcoded = textcoded.replace(/ai/igm,"a");
+    var textcoded = textcoded.replace(/ufat/igm,"u");
+
+    document.getElementById("txtencriptado").innerHTML = textcoded;
+
+
+}
+
+function copy() {
+    var contenido = document.querySelector("#txtencriptado");
+    contenido.select();
+    document.execCommand("copy", false,);
+    alert("¡Hecho!");
+}
